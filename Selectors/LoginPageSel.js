61f63bcd92923915c4eaa0e057d386b752a32ebe
Muhammad Faiq <faiqqadri83@gmail.com>
2023-12-14 13:37:43 +0500

    Logout Test Case

--- Selectors/LoginPageSel.js
+++ Selectors/LoginPageSel.js
@@ -10,6 +10,12 @@
     email: '#email',
     password: '#password',
     loginButton: '[type="submit"]',
+    h1: '.filter-area.mb-5',
+    welcomeAssertion: '[class="filter-area mb-5"] h1',
+    validationAssert: '[class="invalid-feedback"] strong',
+    userIcon: '[id="userAction"]',
+    logoutButton: '[class="bx bx-power-off me-2"]',
+    welcomeBackAssert: '[class="field-area login-area"]',
   };
   
   module.exports= selectors;
--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -1,6 +1,6 @@
 const { test, expect} = require('@playwright/test')
 
-import locators from "../Selectors/LoginPageSel"
+import selectors from "../Selectors/LoginPageSel"
 
 // const selectors = require("../Selectors/LoginPageSel")
 
@@ -8,15 +8,43 @@ test ('Verify that the user is able to login with valid credentials', async ({pa
 
     await page.goto("https://staging.contentatscale.ai/login")
 
-    await page.fill(locators.email, '1t.aamer@gmail.com')
-    await page.fill(locators.password,'!Test123*')
-    await page.click(locators.loginButton)
+    await page.fill(selectors.email, '1t.aamer@gmail.com')
+    await page.fill(selectors.password,'!Test123*')
+    await page.click(selectors.loginButton)
 
-    const header=  page.locator('.filter-area.mb-5');
+    const header =  page.locator(selectors.h1);
     //await expect(header).toContainText('Welcome back, Taimur Aamer!')
+
     await expect(header).toHaveText(/.* Taimur Aamer!/)
-    await expect(page.locator('[class="filter-area mb-5"] h1')).toContainText('Welcome back')
+    await expect(page.locator(selectors.welcomeAssertion)).toContainText('Welcome back')
 
-}
+})
 
-)
+test ('Verify that the user is not able to login with invalid credentials', async ({page}) => {
+
+    await page.goto("https://staging.contentatscale.ai/login")
+
+    await page.fill(selectors.email, '1t.aamer@gmail.comm')
+    await page.fill(selectors.password,'!Test123*1')
+    await page.click(selectors.loginButton)
+
+    const strong = page.locator(selectors.validationAssert)
+    await expect(strong).toHaveText(/.*These credentials do not match our records./)
+
+})
+
+test ('Verify that the user is able to logout from the website', async ({page}) => {
+
+    await page.goto("https://staging.contentatscale.ai/login")
+
+    await page.fill(selectors.email, '1t.aamer@gmail.com')
+    await page.fill(selectors.password,'!Test123*')
+    await page.click(selectors.loginButton)
+
+    await page.click(selectors.userIcon)
+    await page.click(selectors.logoutButton)
+
+    await expect(page.locator(selectors.welcomeBackAssert)).toHaveText(/.* Welcome Back!/)
+    
+    await page.waitForTimeout(5000)
+})
