e3d9fb7c98949da94109e4108c3faacb73d1abfa
Muhammad Faiq <faiqqadri83@gmail.com>
2023-12-20 17:25:04 +0500

    Created Project

--- Selectors/CreatingProjectPage.js
+++ Selectors/CreatingProjectPage.js
@@ -1,8 +1,18 @@
 
+
 module.exports = {
     
     email: '#email',
     password: '#password',
     loginButton: '[type="submit"]',
-
+    openProjectForm: '#open_create_project_form',
+    projectName: '#project-name',
+    projectUrl: '#project-url',
+    projectContext: '#project-context',
+    monthlyPost: '#monthly-post-count',
+    targetAudience: '#target-audience',
+    toneOfVoice: '#tone-of-voice',
+    wordsCount: '#words_count',
+    createProjectButton: '[class="cs-secondary-btn create-project"]',
+    projectNavBar: "//a[contains(.,'Projects')]",
 };
--- tests/createProject.spec.js
+++ tests/createProject.spec.js
@@ -1,20 +1,31 @@
 const {test, expect} = require('@playwright/test')
 
-const createProjectSelectors = require('../Selectors/DashboardPageSel')
-const {email, password, loginButton} = createProjectSelectors 
+const createProjectSelectors = require('../Selectors/CreatingProjectPage.js')
+
+const {email, password, loginButton, openProjectForm, projectName, projectUrl, projectContext, monthlyPost, targetAudience, toneOfVoice, wordsCount, createProjectButton, projectNavBar} = createProjectSelectors 
 
 const randomDate = Date.now()
 const randomName = `testProject${randomDate}`
 
-test ('Verify that the user is able ro create a Project', async ({page}) => {
+test ('Verify that the user is able to create a Project', async ({page}) => {
 
     await page.goto("/login")
     await page.fill(email, '1t.aamer@gmail.com')
     await page.fill(password,'!Test123*')
     await page.click(loginButton)
 
-    await page.click("//a[contains(.,'Projects')]")
-    await page.click('[id="open_create_project_form"]')
-    await page.locator('[id="project-name"]').type(randomName)
-    await page.locator('[id="project-url"]').type
+    await page.locator(projectNavBar).click()
+    await page.waitForTimeout(4000)
+    await page.click(openProjectForm)
+    await page.fill(projectName, randomName)
+    await page.fill(projectUrl, "https://getintopc.com/")
+    await page.fill(projectContext, "Test")
+    await page.fill(monthlyPost, "12")
+    await page.fill(targetAudience, "Software Engineer")
+    
+    await page.selectOption(toneOfVoice, "Persuasive")
+    await page.selectOption(wordsCount, "1,251 to 2,000 words")
+    await page.waitForTimeout(4000)
+    await page.click(createProjectButton)
+    await page.pause(3000)
 })
