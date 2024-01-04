5e3f8d2956e83fa61e090df62f5186c352628db1
muhammadfaiq-af <faiqqadri83@gmail.com>
2024-01-04 16:18:48 +0500

    Latest 04012024

--- tests/deleteProject.spec.js
+++ tests/deleteProject.spec.js
@@ -2,6 +2,7 @@ const { test, expect } = require('@playwright/test')
 
 import { LoginPage } from '../Selectors/Common'
 
+let count;
 const deleteProjectSelectors = require('../Selectors/deleteProjectPage.js')
 
 const { projectNames, deleteProjectBtn, deleteProjectBtnPopup, settingBtn } = deleteProjectSelectors
@@ -10,31 +11,39 @@ test('Verify that the user is able to delete a project', async ({ page }) => {
 
     const loginPage = new LoginPage(page)
 
-    const keywordDelete = "testProject"
     await loginPage.gotoLogin()
     await loginPage.loginBtnClick('1t.aamer@gmail.com', '!Test123*')
 
     await page.click("//a[contains(.,'Projects')]")
     const listOfProjects = '.listing-project table tbody tr'
 
-    let count = await page.locator(listOfProjects).count()
-
-    for (let i = 0; i < count+1; i++) {
-
-        await page.waitForTimeout(7000)
-        count = await page.locator(listOfProjects).count();
-        const getName = await page.locator(listOfProjects).nth(i).locator('td div  strong').textContent()
-
-        if (getName.includes('testProject')) {
-            await page.locator(listOfProjects).nth(i).locator(projectNames).click()
-            await page.waitForTimeout(4000)
-            await page.click(settingBtn)
-            await page.click(deleteProjectBtn)
-            await page.waitForTimeout(1000)
-            await page.click(deleteProjectBtnPopup)
-            continue;
-    }}
-    
-
-
+    while (true) {
+        const count = await page.locator(listOfProjects).count();
+        if (count === 0) {
+          console.log("All matching projects deleted.");
+          break;
+        }
+        let foundMatchingProject = false;
+        for (let i = count - 1; i >= 0; i--) {
+          const getName = await page
+            .locator(listOfProjects)
+            .nth(i)
+            .locator("td div strong")
+            .textContent();
+          if (getName.includes("testProject")) {
+            foundMatchingProject = true;
+            await page.locator(listOfProjects).nth(i).locator(projectNames).click();
+            await page.waitForTimeout(4000);
+            await page.click(settingBtn);
+            await page.click(deleteProjectBtn);
+            await page.waitForTimeout(1000);
+            await page.click(deleteProjectBtnPopup);
+            await page.waitForSelector(listOfProjects, { state: "visible" });
+            break;
+          }
+        }
+        if (!foundMatchingProject) {
+          break;
+        }
+      }
 })
